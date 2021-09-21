747e7c2651d4fd47d3452e3e343ac1022192c884
TravUCD2021 <adamt757@gmail.com>
2021-09-21 18:39:50 +0100

    Update

---
+++ main.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+
+class Assignment3:
+    pass
+
+import matplotlib.pyplot as plt
+
+data=pd.read_csv("Assignment3.csv")
+
+print(data)
+
+y=data["date_time"]
+x=data["amount"]
+y2=data["currency"]
+
+plt.scatter(x,y)
+plt.(x,y)
+
+
+plt.show()
+
+
